280b57c69e6fcf098923612292fe8f006f2d7f90
Sheja <shejadavid11@gmail.com>
2025-03-19 23:53:18 +0400

    Add initial database structure and Car model

---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+namespace DrivingSchool.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        // TODO: Add remaining properties and methods
+    }
+}
