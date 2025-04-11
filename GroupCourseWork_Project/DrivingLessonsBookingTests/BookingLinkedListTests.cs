70dbff3cdc58bd31781dbca89698dbd45a93c41b
Nwukwa Chimdi Nnamdi <114239205+justnnamdi01@users.noreply.github.com>
2025-04-11 12:13:32 +0400

    Update BookingLinkedListTests.cs

--- GroupCourseWork_Project/DrivingLessonsBookingTests/BookingLinkedListTests.cs
+++ GroupCourseWork_Project/DrivingLessonsBookingTests/BookingLinkedListTests.cs
@@ -1,1 +1,46 @@
 // Responsible: Nnamdi
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DrivingLessonsBooking;
+
+namespace DrivingLessonsBookingTests
+{
+    [TestClass]
+    public class CustomLinkedListTests
+    {
+        [TestMethod]
+        public void Insert_ShouldAddElementsInSortedOrder()
+        {
+            var list = new CustomLinkedList<int>();
+            list.Insert(10);
+            list.Insert(5);
+            list.Insert(20);
+
+            Assert.AreEqual(5, list.Search(5));
+            Assert.AreEqual(10, list.Search(10));
+            Assert.AreEqual(20, list.Search(20));
+        }
+
+        [TestMethod]
+        public void Search_ShouldReturnElementIfExists()
+        {
+            var list = new CustomLinkedList<string>();
+            list.Insert("Alice");
+            list.Insert("Bob");
+
+            Assert.AreEqual("Alice", list.Search("Alice"));
+            Assert.AreEqual("Bob", list.Search("Bob"));
+        }
+
+        [TestMethod]
+        public void Delete_ShouldReturnFalseIfElementNotFound()
+        {
+            var list = new CustomLinkedList<int>();
+            list.Insert(1);
+            list.Insert(2);
+
+            bool result = list.Delete(3);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
